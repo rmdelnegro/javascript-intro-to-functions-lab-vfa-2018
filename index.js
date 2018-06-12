df42e2ecba1d384e28bb213658a27593d269bebf
Ralph Del Negro <ralphmdelnegro@gmail.com>
2018-06-12 20:39:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }else if(string === string.toLowerCase){
     return "I can't hear you!"
-  }else{
+  }else if(string === "I love you, Grandma."){
+    return 
   }
-  } 
 }
